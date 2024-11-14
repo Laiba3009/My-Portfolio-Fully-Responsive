07792b84d1dd7024f4227bad7c4398bc666cbd0e
laibajaweed <sarwarilaiba007@gmail.com>
2024-11-14 23:25:24 +0500

    commit

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -19,12 +19,14 @@ function Project(){
         />
         <h3 className="box-nam">Countdown Timer</h3>
         <p className="pp-tex">A Next.js  and Typescript powered  website to truck time with an interactive countdown feature</p>
+        <div className="pro-icon">
         <a className="click" href="https://github.com/Laiba3009/Countdown-Timer-Project">
         <FaGithub  className='p-icon'/>
         </a>
         <a  className= "click" href="https://countdown-timer-project-psi.vercel.app/">
         <TbBrandVercel className="p-icon" />
         </a>
+        </div>
      </div>
      <div className="box">
      <Image className="box-imag" src={"/image/virtual assistant.png"} 
@@ -36,13 +38,14 @@ function Project(){
         <p className="pp-tex">Alexa your virtual assistant built with HTML CSS and JavaScript.
          Alexa is a powerful tool designed.
         </p>
+        <div className="pro-icon">
         <a  className="click" href="https://github.com/Laiba3009/My-Virtual-Assistant">
         <FaGithub  className='p-icon'/>
         </a>
         <a  className= "click" href="https://my-virtual-assistant-rust.vercel.app/">
         <TbBrandVercel className="p-icon" />
         </a>
-
+        </div>
      </div>
      <div className="box">
      <Image className="box-imag" src={"/image/portfolio.png"} 
@@ -52,12 +55,15 @@ function Project(){
         />
         <h3 className="box-nam">Portfolio</h3>
         <p className="pp-tex">My Portfolio fully responsive Animation creating a HTML CSS and JAVASCRIPT website.</p>
-        <a  className="click" href="https://github.com/Laiba3009/My-Portfolio">
+       
+       <div className="pro-icon">
+          <a  className="click" href="https://github.com/Laiba3009/My-Portfolio">
         <FaGithub  className='p-icon'/>
         </a>
         <a  className= "click" href="https://my-portfolio-three-rho-81.vercel.app/">
         <TbBrandVercel className="p-icon" />
         </a>
+        </div>
      </div>
         </div>
         </div>
